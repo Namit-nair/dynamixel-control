6c06220062b185e65e419f0bc0f6a4ba1b3837f8
namit <namithnair14@gmail.com>
2026-07-19 16:30:00 +0530

    Retire Single_Motor_gui.py in favor of the control center
    Its hardcoded PORT_NAME/BAUDRATE/DXL_ID/mode is replaced by dynamixel_control_center.py. Update run_gui.sh to launch the new entry point.

--- run_gui.sh
+++ run_gui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Simple script to run the Dynamixel Control GUI
-source venv/bin/activate
-python3 Single_Motor_gui.py
+# Simple script to run the Dynamixel Control Center
+source .venv/bin/activate
+python3 dynamixel_control_center.py
